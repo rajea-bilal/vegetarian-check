79189926f67e195d104fd5dfecf8c297f574b489
Rajea Bilal <rajea.bilal@gmail.com>
2022-04-19 13:54:53 +0100

    ..

--- template/js/main.js
+++ template/js/main.js
@@ -23,6 +23,7 @@ const url = `https://world.openfoodfacts.org/api/v0/product/${input}.json`
        const item = new ProductInfo(data.product) 
        item.showInfo()
        item.listIngredients()
+       item.listBrand()
     } else if (data.status === 0) {
       alert(`Product ${input} not found. Please try another.`)
     }
@@ -39,35 +40,60 @@ class ProductInfo {
       this.name = productData.product_name
       this.ingredients = productData.ingredients
       this.image = productData.image_url
+      this.brand = productData.brands
     }
 
     showInfo() {
         document.getElementById('product-img').src = this.image
         document.getElementById('product-name').innerText = this.name
     }
+    listBrand() {
+        document.getElementById('brand-name').innerText = this.brand
+    }
 
     listIngredients() {
         let tableRef = document.getElementById('ingredient-table')
-        
-        for(let key in this.ingredients) {
-            let newRow = tableRef.insertRow(-1)
-            let newICell = newRow.insertCell(0)
-            let newVCell = newRow.insertCell(1)
-            let newIText = document.createTextNode(this.ingredients[key].text)
 
-            let vegStatus = this.ingredients[key].vegetarian
-            let newVText = document.createTextNode(vegStatus)
-            newICell.appendChild(newIText) 
-            // putting ingredient text in the ingredient cell
-            newVCell.appendChild(newVText)
-             // putting vegetarian text in the vegetarian cell
+        for(let i = 1; i < tableRef.rows.length;) {
+            tableRef.deleteRow(i);
         }
- 
+        
+        if(!(this.ingredients == null)) {
+
+            for(let key in this.ingredients) {
+                let newRow = tableRef.insertRow(-1)
+                let newICell = newRow.insertCell(0)
+                let newVCell = newRow.insertCell(1)
+                let newVgCell = newRow.insertCell(2)
+               
+               
+                let veganStatus = this.ingredients[key].vegan == null ? 'unknown' : this.ingredients[key].vegan
+                let vegStatus = this.ingredients[key].vegetarian == null ? 'unknown' : this.ingredients[key].vegetarian
+    
+                let newIText = document.createTextNode(this.ingredients[key].text)
+                let newVText = document.createTextNode(vegStatus)
+                let newVgText = document.createTextNode(veganStatus)
+    
+                newICell.appendChild(newIText) 
+                // putting ingredient text in the ingredient cell
+                newVCell.appendChild(newVText)
+                 // putting vegetarian text in the vegetarian cell
+                newVgCell.appendChild(newVgText)
+                 // putting vegan text in the vegan cell
+            }
+
+        }
+        
+       
+    }
+   
+}
+
+
+        
        
 
-    }
 
-  }
 
 // 011110038364
 // 041196910759
